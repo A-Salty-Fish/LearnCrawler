61f5efce8a0e1648ec9c68fb1b5d69b4e5dada54
A-Salty-Fish <1309032417@qq.com>
2020-08-10 16:59:01 +0800

    爬第一个标题

--- 2.3.1/2.3.1.py
+++ 2.3.1/2.3.1.py
@@ -1,10 +1,13 @@
 #/user/bin/python
 #coding: utf-8
 import requests  # 引入包requests
+from bs4 import BeautifulSoup
 
 link = "http://www.santostang.com/"  # 定义link为目标网页地址
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 
 r = requests.get(link, headers=headers)  # 请求网页
-print(r.text)  # r.text是获取的网页内容代码
+soup = BeautifulSoup(r.text, "html.parser")
+title = soup.find("h1", class_="post-title").a.text.strip()
+print(title)
